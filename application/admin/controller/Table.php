5c60790a54aceb50b42e7f3f1354dfaca445cbd3
skyselang <215817969@qq.com>
2019-01-09 16:07:07 +0800

    table组件

--- application/admin/controller/Table.php
+++ application/admin/controller/Table.php
@@ -20,8 +20,9 @@ class Table extends Common
             $is_frame = $this->request->param('is_frame/s');
             $field = $this->request->param('field/s');
             $field_val = $this->request->param('field_val/s');
-            $time_type = $this->request->param('time_type/s');//日期类型
-            $time_date = $this->request->param('time_date/s');//日期选择
+            $date_type = $this->request->param('date_type/s');
+            $start_date = $this->request->param('start_date/s');
+            $end_date = $this->request->param('end_date/s');
             if ($type) {
                 $where[] = ['type', '=', $type];
             }
@@ -31,11 +32,10 @@ class Table extends Common
             if ($field_val) {
                 $where[] = [$field, 'like' ,'%'.$field_val.'%'];
             }
-            if ($time_date) {
-                $time_date = explode(' - ', $time_date);
-                $start_time = strtotime($time_date[0].' 00:00:00');
-                $end_time   = strtotime($time_date[1].' 23:59:59');
-                $where[] = [$time_type, ['>=', $start_time], ['<=', $end_time], 'and'];
+            if ($start_date && $end_date) {
+                $start_time = strtotime($start_date.' 00:00:00');
+                $end_time = strtotime($end_date.' 23:59:59');
+                $where[] = [$date_type, ['>=', $start_time], ['<=', $end_time], 'and'];
             }
             $where[] = ['is_dele', '=', 1];//1正常-1删除
 
@@ -52,7 +52,7 @@ class Table extends Common
             // 数据分页
             $data = $table
                 ->where($where)
-                ->field('id,title,author,keywords,type,look,sort,is_dele,is_frame,create_time,update_time')
+                ->field('id,image,title,author,keywords,description,type,look,sort,is_dele,is_frame,create_time,update_time')
                 ->page($page,$limit)
                 ->order($order)
                 ->select(); 
@@ -61,6 +61,7 @@ class Table extends Common
             if ($data) {
                 $type_arr = array('图文','链接','图集');//新闻类型
                 foreach ($data as $k => $v) {
+                    $data[$k]['image'] = '/static/img/logo.jpg';
                     $data[$k]['type'] = $type_arr[$v['type']-1];
                     $data[$k]['create_time'] = date('Y-m-d H:i:s',$v['create_time']);
                     $data[$k]['update_time'] = date('Y-m-d H:i:s',$v['update_time']);
