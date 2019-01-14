ca03dee4f80d00d7aa5e86bd07d08411ff12b3c2
skyseelang <215817969@qq.com>
2019-01-14 20:25:18 +0800

    添加资料和密码修改

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -11,10 +11,9 @@ class Admin extends Common
     public function info()
     {
     	$admin_id = Session::get('admin_id');
-    	$table = Db::name('admin');
 
     	if ($admin_id) {
-    		$admin = $table
+    		$admin = Db::name('admin')
     			->where('admin_id', $admin_id)
     			->find();
     	} else {
@@ -24,32 +23,40 @@ class Admin extends Common
     	// 修改资料
     	$is_ajax = $this->request->isAjax();
     	if ($is_ajax) {
-    		$nickname = $this->request->param('nickname/s');
-    		$email = $this->request->param('email/s');
+            $admin_id = Session::get('admin_id');
+            if ($admin_id) {
+                $nickname = $this->request->param('nickname/s');
+                $email = $this->request->param('email/s');
 
-    		if ($nickname == '' || $nickname == null || $nickname == 'undefined') {
-    			$res['code'] = 1;
-    			$res['msg'] = '请输入昵称';
-    		} elseif ($email == '' || $email == null || $email == 'undefined') {
-    			$res['code'] = 1;
-    			$res['msg'] = '请输入邮箱';
-    		} else {
-    			$data['nickname'] = $nickname;
-    			$data['email'] = $email;
-    			$data['create_time'] = time();
+                if ($nickname == '' || $nickname == null || $nickname == 'undefined') {
+                    $res['code'] = 1;
+                    $res['msg'] = '请输入昵称';
+                } elseif ($email == '' || $email == null || $email == 'undefined') {
+                    $res['code'] = 1;
+                    $res['msg'] = '请输入邮箱';
+                } else {
+                    
+                    $data['nickname'] = $nickname;
+                    $data['email'] = $email;
+                    $data['update_time'] = time();
 
-    			$update_res = $table
-    				->where('admin_id', $admin_id)
-    				->update($data);
+                    $update = Db::name('admin')
+                        ->where('admin_id', $admin_id)
+                        ->update($data);
 
-    			if ($update_res) {
-    				$res['code'] = 0;
-    				$res['msg'] = '修改成功';
-    			} else {
-    				$res['code'] = 1;
-    				$res['msg'] = '修改失败';
-    			}
-    		}
+                    if ($update) {
+                        $res['code'] = 0;
+                        $res['msg'] = '修改成功';
+                    } else {
+                        $res['code'] = 1;
+                        $res['msg'] = '修改失败';
+                    }
+                }
+            } else {
+                $res['code'] = 1;
+                $res['msg'] = '修改失败';
+            }
+    		
     		return json($res);
     	}
     	
@@ -57,8 +64,64 @@ class Admin extends Common
     	return $this->fetch();
     }
 
-    public function password()
+    public function pwd()
     {
+        // 修改密码
+        $is_ajax = $this->request->isAjax();
+        if ($is_ajax) {
+            $admin_id = Session::get('admin_id');
+            if ($admin_id) {
+                $oldpwd = $this->request->param('oldpwd/s');
+                $newpwd = $this->request->param('newpwd/s');
+                $newpwds = $this->request->param('newpwds/s');
+
+                if ($oldpwd == '' || $oldpwd == null || $oldpwd == 'undefined') {
+                    $res['code'] = 1;
+                    $res['msg'] = '请输入原密码';
+                } elseif ($newpwd == '' || $newpwd == null || $newpwd == 'undefined') {
+                    $res['code'] = 1;
+                    $res['msg'] = '请输入新密码';
+                } elseif ($newpwds == '' || $newpwds == null || $newpwds == 'undefined') {
+                    $res['code'] = 1;
+                    $res['msg'] = '请再次输入新密码';
+                } elseif ($newpwd !== $newpwds) {
+                    $res['code'] = 1;
+                    $res['msg'] = '请新密码与确认密码不一致';
+                } else {
+                    $where['admin_id'] = $admin_id;
+                    $where['password'] = md5($oldpwd);
+
+                    $oldpwd_check = Db::name('admin')
+                        ->where($where)
+                        ->find();
+
+                    if ($oldpwd_check) {
+                        $data['password'] = md5($newpwds);
+                        $data['update_time'] = time();
+
+                        $update = Db::name('admin')
+                            ->where('admin_id', $admin_id)
+                            ->update($data);
+
+                        if ($update) {
+                            $res['code'] = 0;
+                            $res['msg'] = '修改成功';
+                        } else {
+                            $res['code'] = 1;
+                            $res['msg'] = '修改失败';
+                        }
+                    } else {
+                        $res['code'] = 1;
+                        $res['msg'] = '原密码错误';
+                    }
+                }
+            } else {
+                $res['code'] = 1;
+                $res['msg'] = '修改失败';
+            }
+            
+            return json($res);
+        }
     	return $this->fetch();
     }
 }
