3bf59a5f4ec5b707729c678ebe2b0dbaf87fccc0
skyseelang <215817969@qq.com>
2019-01-22 22:25:31 +0800

    组件table修改

--- application/admin/controller/Table.php
+++ application/admin/controller/Table.php
@@ -48,15 +48,14 @@ class Table extends Common
                 $order = ['sort'=>'desc','id'=>'desc'];
             }
 
-            $table = Db::name('news');
-            // 数据分页
-            $data = $table
+            // 数据
+            $data = Db::name('news')
                 ->where($where)
                 ->field('id,image,title,author,keywords,description,type,look,sort,is_dele,is_frame,create_time,update_time')
                 ->page($page,$limit)
                 ->order($order)
                 ->select(); 
-            $count = $table->where($where)->count();// 查询总记录数
+            $count = Db::name('news')->where($where)->count();// 查询总记录数
 
             if ($data) {
                 $type_arr = array('图文','链接','图集');//新闻类型
@@ -73,7 +72,7 @@ class Table extends Common
             } else {
                 $res['code'] = 0;
             }
-
+            
             return json($res);
         }
     	
@@ -85,19 +84,72 @@ class Table extends Common
         return $this->fetch();
     }
 
+    /**
+     * 修改
+     * @return json 修改结果
+     */
     public function edit()
     {
-        return $this->fetch();
+        $param = $this->request->param();
+        $id = $param['id'];
+        $field = $param['field'];
+        $value = $param['value'];
+        
+        if ($id && $field && $value) {
+            $update = Db::name('news')
+                ->where('id', $id)
+                ->update([$field => $value]);
+            if ($update) {
+                $res['code'] = 0;
+                $res['msg'] = '修改成功';
+            } else {
+                $res['code'] = 1;
+                $res['msg'] = '修改失败';
+            }
+        } else {
+            $res['code'] = 1;
+            $res['msg'] = '参数错误';
+        }
+        sleep(5);
+        return json($res);
     }
 
+    /**
+     * 删除
+     * @return json 删除结果
+     */
     public function dele()
     {
-        if ($this->request->isAJAX()) {
-            sleep(5);
+        $is_ajax = $this->request->isAJAX();
+
+        if ($is_ajax) {
+            $id = $this->request->param('id/s');
+
+            $strpos = strpos($id, ',');
+            if ($strpos > 0) {
+                $id_arr = explode(',', $id);
+            } else {
+                $id_arr = ['0'=>$id];
+            }
+
+            $success = $fail = 0;
+            foreach ($id_arr as $k => $v) {
+                $dele = Db::name('news')
+                    ->where('id', $v)
+                    ->update(['is_dele' => -1]);
+                if ($dele) {
+                    $success += 1;
+                    Db::name('news')
+                        ->where('id', $v)
+                        ->update(['delete_time' => time()]);
+                } else {
+                    $fail += 1;
+                }
+            }
 
             $res['code'] = 0;
-            $res['msg'] = '删除成功！';
-
+            $res['msg'] = "成功删除{$success}条，失败{$fail}条数据";
+            sleep(5);
             return json($res);
         }
     }
