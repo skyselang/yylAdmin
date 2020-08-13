bfa0fe8de7f9d6e29c036b908263471f461929bf
skyselang <215817969@qq.com>
2020-08-13 20:41:50 +0800

    容器Provider定义文件
    Signed-off-by: skyselang <215817969@qq.com>

--- app/provider.php
+++ app/provider.php
@@ -1,9 +1,14 @@
 <?php
+/*
+ * @Description  : 容器Provider定义文件
+ * @Author       : https://github.com/skyselang
+ * @Date         : 2020-04-16
+ * @LastEditTime : 2020-08-13
+ */
 
 use app\ExceptionHandle;
 use app\Request;
 
-// 容器Provider定义文件
 return [
     'think\Request'          => Request::class,
     'think\exception\Handle' => ExceptionHandle::class,
