3f0c5df0383e63bad6d1e792ed5819a6fe658157
skyselang <215817969@qq.com>
2020-08-13 21:34:25 +0800

    index应用首页
    Signed-off-by: skyselang <215817969@qq.com>

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -3,12 +3,20 @@
  * @Description  : 首页
  * @Author       : https://github.com/skyselang
  * @Date         : 2020-05-17
+ * @LastEditTime : 2020-08-13
  */
 
 namespace app\index\controller;
 
 class Index
 {
+    /**
+     * 首页
+     *
+     * @method GET
+     *
+     * @return json
+     */
     public function index()
     {
         $data['desc']   = 'yylAdmin基于ThinkPHP6和Element2的极简后台管理系统';
