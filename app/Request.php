62683a1e35fb60f3661fac42223350535f8fcefc
skyselang <215817969@qq.com>
2020-08-13 20:42:16 +0800

    应用请求对象类
    Signed-off-by: skyselang <215817969@qq.com>

--- app/Request.php
+++ app/Request.php
@@ -1,8 +1,13 @@
 <?php
+/*
+ * @Description  : 应用请求对象类
+ * @Author       : https://github.com/skyselang
+ * @Date         : 2020-04-16
+ * @LastEditTime : 2020-08-13
+ */
 
 namespace app;
 
-// 应用请求对象类
 class Request extends \think\Request
 {
     protected $filter = ['trim'];
