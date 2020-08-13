6ce2a18eb86452755d44021c727c1d18be2c49fe
skyselang <215817969@qq.com>
2020-08-13 20:43:04 +0800

    系统服务定义文件
    Signed-off-by: skyselang <215817969@qq.com>

--- app/service.php
+++ app/service.php
@@ -1,9 +1,13 @@
 <?php
+/*
+ * @Description  : 系统服务定义文件,服务在完成全局初始化之后执行
+ * @Author       : https://github.com/skyselang
+ * @Date         : 2020-04-16
+ * @LastEditTime : 2020-08-13
+ */
 
 use app\AppService;
 
-// 系统服务定义文件
-// 服务在完成全局初始化之后执行
 return [
     AppService::class,
 ];
