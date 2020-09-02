94495cfff9074177e4c8519a942858298b8e9b97
skyselang <215817969@qq.com>
2020-09-02 22:30:40 +0800

    禁访应用列表
    Signed-off-by: skyselang <215817969@qq.com>

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     // 域名绑定（自动多应用模式有效）
     'domain_bind'      => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
-    'deny_app_list'    => ['cache', 'tool'],
+    'deny_app_list'    => ['cache', 'utils'],
 
     // 异常页面的模板文件
     'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
