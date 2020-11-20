e03d3dfb9bfc7b837d0afc7fddfb11af0c128c80
skyselang <215817969@qq.com>
2020-11-20 20:43:59 +0800

    修改：描述
    Signed-off-by: skyselang <215817969@qq.com>

--- app/admin/service/AdminIndexService.php
+++ app/admin/service/AdminIndexService.php
@@ -3,7 +3,7 @@
  * @Description  : 控制台
  * @Author       : https://github.com/skyselang
  * @Date         : 2020-05-17
- * @LastEditTime : 2020-09-30
+ * @LastEditTime : 2020-11-20
  */
 
 namespace app\admin\service;
@@ -18,9 +18,9 @@ class AdminIndexService
     public static function index()
     {
         $data['name']   = 'yylAdmin';
-        $data['desc']   = '基于ThinkPHP6和Element2的极简后台管理系统';
-        $data['Github'] = 'https://github.com/skyselang/yylAdmin';
-        $data['Gitee']  = 'https://gitee.com/skyselang/yylAdmin';
+        $data['desc']   = '基于ThinkPHP6和Vue2的极简后台管理系统';
+        $data['gitee']  = 'https://gitee.com/skyselang/yylAdmin';
+        $data['github'] = 'https://github.com/skyselang/yylAdmin';
 
         return $data;
     }
